227f63bea1427a13d5d8a67c9e6dc1bea9756575
相蕾 <lei.xiang@99bill.com>
2018-04-19 16:04:25 +0800

    修改内容名

---
+++ Slam小熊/ar/default.lua
@@ -0,0 +1,54 @@
+app_controller = ae.ARApplicationController:shared_instance()
+app_controller:require('./scripts/include.lua')
+app = AR:create_application(AppType.Slam, "bear")
+app:load_scene_from_json("res/simple_scene.json","demo_scene")
+
+scene = app:get_current_scene()
+
+local version = app:get_engine_version()
+local is_anim = true
+local NOAMRL_POD = 1
+local STAR_POD = 2
+local current_pod = NOAMRL_POD
+
+app.on_loading_finish = function()
+	pod_anim_test()
+end
+
+function pod_anim_test()
+	anim = scene.bear:pod_anim()
+					 :anim_repeat(false)
+					 :on_complete(function() 
+					 	ARLOG('pod anim done')
+					 end)
+					 :anim_repeat(true)
+					 :start()
+					 
+	--点击模型打开url
+	scene.bear.on_click = function()
+		app:open_url("com.99bill.kuaiqian")
+	end
+
+	--点击按钮暂停/开始动画
+	scene.pause.on_click = function()
+		if(is_anim)
+		then
+			is_anim = false
+			scene.pause:replace_texture("/res/texture/video_resume.png","uDiffuseTexture")
+			anim:pause()
+		else 
+			is_anim = true
+			scene.pause:replace_texture("/res/texture/video_pause.png","uDiffuseTexture")
+			anim:resume()
+		end
+	end
+end
+
+
+
+
+
+
+
+
+
